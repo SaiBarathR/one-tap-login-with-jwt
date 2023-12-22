88b6ec9b44b3cc099f8ba2a01dfea80dcf65585d
Sai Barath <saibarathr@gmail.com>
2023-12-22 13:27:31 +0530

    Add user service module

---
+++ src/service/user.service.js
@@ -0,0 +1,42 @@
+import { AuthServices } from "./auth.service";
+
+const UserService = (function () {
+    let service = {};
+    const baseUrl = 'http://localhost:8080/api';
+    let headers = { 'Content-Type': 'application/json', }
+    const urlList = {
+        usersInfo: baseUrl + '/userInfo',
+        deleteAccount: (email) => baseUrl + `/deleteAccount/${sessionStorage.getItem('signInType')}/${email}`,
+    }
+
+    service.getUserInfo = function () {
+        const header = { ...headers, 'x-access-token': AuthServices.getAuthToken() }
+        const body = { email: localStorage.getItem('email'), type: sessionStorage.getItem('signInType') }
+        return fetch(urlList.usersInfo, {
+            method: 'POST',
+            headers: header,
+            body: JSON.stringify(body),
+        }).then(response => {
+            return response.json() || response || "No response";
+        }).catch((error) => {
+            return error;
+        });
+    }
+
+    service.deleteAccount = function (email) {
+        console.log('starting to delete', email)
+        const header = { 'x-access-token': AuthServices.getAuthToken() }
+        return fetch(urlList.deleteAccount(email), {
+            method: 'DELETE',
+            headers: header,
+        }).then(response => {
+            return response.json() || response || "No response";
+        }).catch((error) => {
+            return error;
+        });
+    }
+
+    return service;
+}());
+
+export default UserService;
