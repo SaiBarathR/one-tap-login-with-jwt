0ecf5fb703ac77434e9a86ae69af2c71de4195dd
Sai Barath <saibarathr@gmail.com>
2023-12-26 18:16:52 +0530

    Update App component styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-full h-screen text-white">
       <RouterProvider router={router} />
     </div>
   );
