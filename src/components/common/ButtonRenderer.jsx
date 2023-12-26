179a9e6fe457fffb94abe4c4d401afbb01c7f599
Sai Barath <saibarathr@gmail.com>
2023-12-26 16:34:22 +0530

    Add loading prop to ButtonRenderer component

--- src/components/common/ButtonRenderer.jsx
+++ src/components/common/ButtonRenderer.jsx
@@ -1,11 +1,12 @@
 import { Button } from "@nextui-org/react";
 
-export default function ButtonRenderer({ className, text, imgSrc, onClickAction, ...additionalProps }) {
+export default function ButtonRenderer({ className, loading = false, text, imgSrc, onClickAction, ...additionalProps }) {
     return (
         <Button className={`bg-[#2D3B48] text-white flex justify-center items-center gap-2 rounded-xl px-2 py-1 hover:scale-105 transition ease-in-out delay-75 ${className}`}
             onClick={onClickAction}
             endContent={imgSrc && <img className="w-5 h-5" src={imgSrc} alt={text} />}
             {...additionalProps}
+            isLoading={loading}
         >
             {text}
         </Button>
