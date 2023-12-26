ddbd2e00b592ae956a0bc4f1971b543010903f2e
Sai Barath <saibarathr@gmail.com>
2023-12-26 18:17:35 +0530

    Refactor UserInfoRenderer component to include a loading skeleton

--- src/components/Home/UserInfoRenderer.jsx
+++ src/components/Home/UserInfoRenderer.jsx
@@ -1,12 +1,13 @@
 import { Avatar } from "@nextui-org/react";
 import Logout from "../Logout"
 import UserService from "../../service/user.service"
+import { Skeleton } from "@nextui-org/react";
 import { AuthServices } from "../../service/auth.service"
 import { useNavigate } from "react-router";
 import ButtonRenderer from "../common/ButtonRenderer";
 import { useState } from "react";
 
-export default function UserInfoRenderer({ userDetails }) {
+export default function UserInfoRenderer({ userDetails, loadingUser }) {
     const navigateTo = useNavigate();
     const signInType = sessionStorage.getItem('signInType');
     const [loading, setLoading] = useState(false);
@@ -34,22 +35,38 @@ export default function UserInfoRenderer({ userDetails }) {
 
     function InfoRenderer({ name, value }) {
         return (
-            <div className="flex flex-row gap-5 rounded-xl shadow-md p-2 w-full bg-slate-200">
-                <h1 className={"text-xl font-medium text-[#2D3B48] self-start "}>{name}</h1>
-                <h1 className={`text-xl text-[#2D3B48] self-start ${name === 'Name:' && 'capitalize'}`}>{value}</h1>
+            <div className="flex flex-row gap-5 rounded-xl shadow-md p-2 w-full bg-slate-600">
+                <h1 className={"text-xl font-medium self-start "}>{name}</h1>
+                <h1 className={`text-xl  self-start ${name === 'Name:' && 'capitalize'}`}>{value}</h1>
             </div>
         )
     }
 
+    const UserInfoSkeleton = () => {
+        return (
+            <>
+                <Skeleton className="flex rounded-full w-20 h-20" />
+                <Skeleton className="flex rounded-xl shadow-md p-2 h-10 w-full bg-slate-600" />
+                <Skeleton className="flex rounded-xl shadow-md p-2 h-10 w-full bg-slate-600" />
+                <Skeleton className="flex rounded-xl shadow-md p-2 h-10 w-full bg-slate-600" />
+                <Skeleton className="flex rounded-xl shadow-md p-2 h-10 w-full bg-slate-600" />
+            </>
+        )
+    }
+
     return (
-        <div className="flex flex-col gap-5 min-w-[50%]  bg-white rounded-md shadow-md p-5">
-            <h1 className="text-4xl font-medium text-[#2D3B48] ">User Profile</h1>
+        <div className="flex flex-col gap-5 min-w-[50%] card rounded-md shadow-md p-5">
+            <h1 className="text-4xl font-medium text-[#b3c1ce] ">User Profile</h1>
             <div className="flex flex-col gap-5 justify-center w-full items-center">
-                <Avatar className="flex rounded-full w-20 h-20" src={userDetails.photo || userDetails.picture} />
-                {userDetails.name && <InfoRenderer name="Name:" value={userDetails.name} />}
-                {userDetails.age && <InfoRenderer name="Age:" value={userDetails.age || '-'} />}
-                {userDetails.gender && <InfoRenderer name="Gender:" value={userDetails.gender} />}
-                {userDetails.email && <InfoRenderer name="Email:" value={userDetails.email} />}
+                {
+                    loadingUser ? <UserInfoSkeleton /> : <>
+                        <Avatar className="flex rounded-full w-20 h-20" src={userDetails.photo || userDetails.picture} />
+                        {userDetails.name && <InfoRenderer name="Name:" value={userDetails.name} />}
+                        {userDetails.age && <InfoRenderer name="Age:" value={userDetails.age || '-'} />}
+                        {userDetails.gender && <InfoRenderer name="Gender:" value={userDetails.gender} />}
+                        {userDetails.email && <InfoRenderer name="Email:" value={userDetails.email} />}
+                    </>
+                }
             </div>
             <div className={`w-full flex ${(signInType === 'manual' || signInType === 'google') ? 'justify-between' : 'justify-end'}`}>
                 {(signInType === 'manual' || signInType === 'google') && <ButtonRenderer loading={loading} text={'Delete Account'} onClick={deleteAccount} />}
