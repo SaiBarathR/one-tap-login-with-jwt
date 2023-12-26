3a1633955ab25d40a5599ead7ef62ac3585e0cce
Sai Barath <saibarathr@gmail.com>
2023-12-26 13:59:17 +0530

    Refactor ButtonRenderer component to use NextUI Button

--- src/components/common/ButtonRenderer.jsx
+++ src/components/common/ButtonRenderer.jsx
@@ -1,11 +1,13 @@
+import { Button } from "@nextui-org/react";
+
 export default function ButtonRenderer({ className, text, imgSrc, onClickAction, ...additionalProps }) {
     return (
-        <button className={`bg-[#2D3B48] text-white flex justify-center items-center gap-2 rounded-xl px-2 py-1 hover:scale-105 transition ease-in-out delay-75 ${className}`}
+        <Button size="sm" className={`bg-[#2D3B48] text-white flex justify-center items-center gap-2 rounded-xl px-2 py-1 hover:scale-105 transition ease-in-out delay-75 ${className}`}
             onClick={onClickAction}
+            endContent={imgSrc && <img className="w-5 h-5" src={imgSrc} alt={text} />}
             {...additionalProps}
         >
-            {imgSrc && <img className="w-5 h-5" src={imgSrc} alt={text} />}
             {text}
-        </button>
+        </Button>
     )
 }
