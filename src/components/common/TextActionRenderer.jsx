c51756e288b805f70c89efbf2d9ba49bd0ff9c11
Sai Barath <saibarathr@gmail.com>
2023-12-26 18:17:13 +0530

    Update text action renderer styles

--- src/components/common/TextActionRenderer.jsx
+++ src/components/common/TextActionRenderer.jsx
@@ -1,6 +1,6 @@
 export default function TextActionRenderer({ text, action, onClickAction }) {
     return (
-        <label className="text-[#2D3B48] text-sm font-medium flex gap-1 justify-center">
+        <label className="text-[#adafb1] text-sm font-medium flex gap-1 justify-center">
             {text}
             <span className="text-[#008dff] cursor-pointer text-sm font-medium" onClick={onClickAction}>
                 {action}
