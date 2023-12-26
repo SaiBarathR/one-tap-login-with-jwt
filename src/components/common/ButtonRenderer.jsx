36d058a51a5b28dce378993aeb75e912191bffd5
Sai Barath <saibarathr@gmail.com>
2023-12-26 18:17:04 +0530

    Add color prop to ButtonRenderer component

--- src/components/common/ButtonRenderer.jsx
+++ src/components/common/ButtonRenderer.jsx
@@ -4,6 +4,7 @@ export default function ButtonRenderer({ className, loading = false, text, imgSr
     return (
         <Button className={`bg-[#2D3B48] text-white flex justify-center items-center gap-2 rounded-xl px-2 py-1 hover:scale-105 transition ease-in-out delay-75 ${className}`}
             onClick={onClickAction}
+            color="primary"
             endContent={imgSrc && <img className="w-5 h-5" src={imgSrc} alt={text} />}
             {...additionalProps}
             isLoading={loading}
