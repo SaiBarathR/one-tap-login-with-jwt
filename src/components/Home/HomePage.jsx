7440349739bcf40c44f70a9b0a49764c4e7abba3
Sai Barath <saibarathr@gmail.com>
2023-12-26 18:17:28 +0530

    Refactor HomePage component

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,4 +1,3 @@
-import UserInfoSkeleton from "../common/loading/UserInfoSkeleton";
 import UserInfoRenderer from "./UserInfoRenderer";
 import { useUserDetails } from "../hooks/useUserDetails";
 
@@ -7,11 +6,11 @@ export default function HomePage() {
     const { userDetails, loading } = useUserDetails();
 
     return (
-        <div className="w-full h-screen">
-            <div className="flex flex-col gap-5 h-screen w-full  bg-[#F2F4F3] items-center justify-center">
-                {loading ? <UserInfoSkeleton /> : userDetails ? <UserInfoRenderer userDetails={userDetails} /> :
-                    <div className="flex flex-col min-w-[50%] h-1/2 bg-white rounded-md shadow-md  items-center justify-center">
-                        <h1 className="text-2xl flex font-medium text-[#2D3B48]">{'No Data to display :('}</h1>
+        <div className="card w-full h-screen">
+            <div className="flex flex-col gap-5 h-screen w-full  items-center justify-center">
+                {(userDetails || loading) ? <UserInfoRenderer userDetails={userDetails} loadingUser={loading} /> :
+                    <div className="card flex flex-col min-w-[50%] h-1/2  rounded-md shadow-md  items-center justify-center">
+                        <h1 className="text-2xl flex font-medium text-[#b3b5b6]">{'No Data to display :('}</h1>
                     </div>
                 }
             </div>
